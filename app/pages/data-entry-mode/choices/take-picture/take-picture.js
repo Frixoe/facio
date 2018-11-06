8fab4a2c1ca94bbd0e372352a8164a1bda7ddf86
Frix <suryansh5060@gmail.com>
2018-11-05 22:56:58 -0800

    Add webcam functionality

--- app/pages/data-entry-mode/choices/take-picture/take-picture.js
+++ app/pages/data-entry-mode/choices/take-picture/take-picture.js
@@ -3,9 +3,24 @@ const h = require("./../../../../helpers/getRendererModules")(false, false, [
     "switchPage"
 ]);
 
+const WebCam = require("webcamjs");
+
 h.logger.log("loaded 'take-picture.html'");
 
 $(() => {
+    WebCam.set({
+        width: 400,
+        height: 400,
+        fps: 30
+    })
+    WebCam.attach("#video");
+
+    $("#take-snap").click(() => {
+        WebCam.snap(uri => {
+            document.getElementById("snap").src = uri;
+        });
+    });
+    
     $("#back-btn").click(() => h.switchPage(fadeOutLeft, "choices.html"));
 
     $(".container").show().addClass("fadeInLeft animated");
