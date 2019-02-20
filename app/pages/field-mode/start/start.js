d6044761f57160ab0215e8177a6af6305ab94776
Frixoe <suryansh5060@gmail.com>
2019-02-20 09:33:03 +0530

    Initial lines for camvas test

--- app/pages/field-mode/start/start.js
+++ app/pages/field-mode/start/start.js
@@ -8,6 +8,12 @@ const h = require("./../../../helpers/getRendererModules")(false, false, [
 $(() => {
     $("#back-btn").click(() => h.switchPage(fadeOutRight, "field.html", true));
 
+    let ctx = document.getElementsByTagName('canvas')[0].getContext('2d');
+    let draw = function(video, dt) {
+      ctx.drawImage(video, 0, 0);
+    }
+    let myCamvas = new camvas(ctx, draw);
+
     $(".container")
         .show()
         .addClass("fadeInRight animated");
