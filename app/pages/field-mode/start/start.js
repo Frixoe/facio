248f7199e4b32940b377cea15baa8d27e1b49603
Frixoe <suryansh5060@gmail.com>
2019-02-26 14:08:01 +0530

    Finish start.html basic layout and functionality
    -> Basically just copied the code from take-picture.html/js and pasted
    in start.html/js.
    -> Need to figure out an easy way to integrate React with Electron.

--- app/pages/field-mode/start/start.js
+++ app/pages/field-mode/start/start.js
@@ -4,15 +4,65 @@ const h = require("./../../../helpers/getRendererModules")(false, false, [
     "switchPage",
     "fs"
 ]);
+const wc = require("webcamjs");
+
+let divWidth = window.innerWidth;
+let divHeight = window.innerHeight;
+
+h.logger.log("window dims: " + window.innerWidth + " " + window.innerHeight);
+
+wc.set({
+    width: divWidth,
+    height: divHeight,
+    image_format: "png",
+    flip_horiz: true
+});
+
+wc.on("load", () => {
+    h.logger.log("webcamjs loaded...");
+});
 
 $(() => {
-    $("#back-btn").click(() => h.switchPage(fadeOutRight, "field.html", true));
+    $(".back-btns").click((e) => {
+        e.preventDefault();
+        h.switchPage(fadeOutRight, "field.html", true)
+    });
 
-    let ctx = document.getElementsByTagName('canvas')[0].getContext('2d');
-    let draw = function(video, dt) {
-      ctx.drawImage(video, 0, 0);
-    }
-    let myCamvas = new camvas(ctx, draw);
+    $("#video").attr({
+        style: "width: " + divWidth + "px; " + "height: " + divHeight + "px;"
+    });
+
+    wc.on("error", err => {
+        $("#recheck-webcam-btn").prop("disabled", false);
+
+        h.logger.log("err: " + err);
+
+        let p = document.createElement("p");
+        p.innerHTML = err;
+
+        $("#info-text")
+            .html("")
+            .append(p);
+    });
+
+    wc.on("live", () => {
+        $("#recheck-webcam-btn").prop("disabled", true);
+
+        fadeOutDown();
+        toggleWebcam();
+        h.logger.log("found webcam, starting streaming...");
+    });
+
+    wc.attach("#video");
+
+    $("#recheck-webcam-btn").click(() => {
+        $("#info-text")
+            .html("")
+            .html("Loading...");
+
+        wc.reset();
+        wc.attach("#video");
+    });
 
     $(".container")
         .show()
@@ -29,4 +79,30 @@ function fadeOutRight() {
     $(".container")
         .removeClass(["animated", "fadeInRight"])
         .addClass("fadeOutRight animated");
+}
+
+function toggleWebcam() {
+    if ($(".webcam").hasClass("fadeInDown"))
+        $(".webcam")
+            .removeClass(["fadeInDown", "animated"])
+            .addClass("fadeOutUp animated")
+            .toggle();
+    else
+        $(".webcam")
+            .removeClass(["animated", "fadeOutUp"])
+            .addClass("fadeInDown animated")
+            .toggle();
+}
+
+function fadeOutDown() {
+    if ($(".container").hasClass("fadeInRight"))
+        $(".container")
+            .removeClass(["animated", "fadeInRight"])
+            .addClass("fadeOutDown animated")
+            .hide();
+    else
+        $(".container")
+            .removeClass(["animated", "fadeInUp"])
+            .addClass("fadeOutDown animated")
+            .hide();
 }
